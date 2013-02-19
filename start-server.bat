d9de605f5beb83a558198c4f5c1c3fe1b7eb012e
Sean Dawson <sean.dawson@noxharmonium.com>
2013-02-19 19:38:19 +1100

    Added python version detection to windows start-server script.

--- start-server.bat
+++ start-server.bat
@@ -1,1 +1,15 @@
+@ECHO OFF
+REM Run this on windows to open up a python webserver
+REM Requires python. Will try to determine which
+REM webserver module to use.
+
+echo Trying the webserver module for python 2.x...
 python -m SimpleHTTPServer
+IF NOT %ERRORLEVEL%==0 GOTO VER3
+GOTO End
+
+:VER3
+echo That module didn't work. Trying the one for python 3.x...
+python -m http.server
+
+:End

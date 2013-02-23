7d0db86f4d0087dcc5b13d437f7fad972cffa304
Sean Dawson <sean.dawson@noxharmonium.com>
2013-02-23 14:03:19 +1100

    Added new example js file.

---
+++ canvas/js/examples/myFirstCanvas.js
@@ -0,0 +1,14 @@
+///
+/// myFirstCanvas.js
+///
+/// Basic canvas example.
+///
+function draw(canvas)
+{	
+	helper.clear(canvas) // Clear canvas and set size
+   	var ctx=canvas[0].getContext("2d");	
+}
+
+
+
+
